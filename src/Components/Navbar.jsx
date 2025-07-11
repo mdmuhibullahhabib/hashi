94a529771f895933790f9cbc15a6f9b0bea9f47e
Md Muhib Ullah Habib <mdmuhibullahhabib6348@gmail.com>
2025-07-11 23:58:39 +0600

    nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ import useRole from '../hooks/useRole';
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
 
-  co
+  c
 const [isRole] = useRole()
   
   const handleLogout = () => {
