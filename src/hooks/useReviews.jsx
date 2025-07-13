1e1217409cb4f38f7ca3585f04f294682a80bc78
Md Muhib Ullah Habib <mdmuhibullahhabib6348@gmail.com>
2025-07-14 00:33:58 +0600

    reviews

--- src/hooks/useReviews.jsx
+++ src/hooks/useReviews.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
+import { AuthContext } from '../Provider/Authprovider';
 
 const useReviews = () => {
 
     const axiosSecure = useAxiosSecure();
+    const {user} = useContext(AuthContext);
 
     const { data: reviews = [], refetch } = useQuery({
         queryKey: ['review'],
         queryFn: async () => {
-            const res = await axiosSecure.get(`/booked?email=${user.email}`)
-
-            const res = await axiosSecure.get(`/reviews`)
+            const res = await axiosSecure.get(`/reviews?email=${user.email}`)
+            // const res = await axiosSecure.get(`/reviews`)
             return res.data;
         }
     })
