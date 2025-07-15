c5ddb7ae4836468668caa81ec3ac57c091cd7fb4
Md Muhib Ullah Habib <mdmuhibullahhabib6348@gmail.com>
2025-07-15 21:39:30 +0600

    appoiuntment

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React from 'react'
 
  const axiosPublic = axios.create({
-    baseURL: 'https://doc-house-server-weld.vercel.app'
+    baseURL: ''
 })
 
 const useAxiosPublic = () => {
