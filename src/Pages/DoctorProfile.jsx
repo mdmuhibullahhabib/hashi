cd7d387b058d7e668e0735a7a2973ca10269b4f1
Md Muhib Ullah Habib <mdmuhibullahhabib6348@gmail.com>
2025-07-16 20:46:47 +0600

    doctors

--- src/Pages/DoctorProfile.jsx
+++ src/Pages/DoctorProfile.jsx
@@ -12,7 +12,7 @@ const DoctorProfile = () => {
 
   const { id } = useParams();
   const [doctors] = useDoctor();
-  console.lo
+  console.l
 
   const doctor = doctors.find(doc => doc._id === id);
 
