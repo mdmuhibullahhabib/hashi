6d37af87edcce0c980300e3bc19d9f2c860329c8
Md Muhib Ullah Habib <mdmuhibullahhabib6348@gmail.com>
2025-07-18 16:21:37 +0600

    about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -64,7 +64,7 @@ const About = () => {
                 </div>
 
                 {/* Team Section */}
-                <div className="py-16 px-6 max-w-6xl mx-auto text-center">
+                <div className="py-16 px-6 max-w-6xl er">
 
                 </div>
 
